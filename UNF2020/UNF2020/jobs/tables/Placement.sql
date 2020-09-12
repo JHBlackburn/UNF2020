c54e6e0d3bd85b844e4c073e5fb0223597cc74d1
R <roman.rojas@suddath.com>
2020-09-12 12:08:48 -0400

    first commit

---
+++ UNF2020/UNF2020/jobs/tables/Placement.sql
@@ -0,0 +1,13 @@
+﻿create table jobs.Placement
+(
+	Id int not null primary key identity,
+	PersonFirstName varchar (100) null,
+	PersonLastName	varchar (100) null,
+	CompanyName		varchar (100) null,
+	CompanyCity		varchar (100) null,
+	CompanyState	varchar (100) null,
+	StartYearlySalary decimal (14, 2) null,
+	StartDate date null,
+	EndDate date null
+)
+
